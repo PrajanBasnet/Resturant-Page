44b74befb696ef6e46148cf38ea28ec20d098a85
PrajanBasnet <spiderbasnet140@gmail.com>
2025-05-01 18:39:42 +0545

    css for mai

--- src/menu.js
+++ src/menu.js
@@ -2,32 +2,35 @@ import FoodBg from "./img/back.jpg"
 import "./css/menu.css"
 let mainContent = document.querySelector("#content");
 
-function centerText(){
+function centerText() {
     const h1 = document.createElement("h1");
     h1.innerHTML = "Astra Restrurant <b>Menu</b>"
     mainContent.appendChild(h1)
 }
 const newDiv = document.createElement("div");
-newDiv.setAttribute("id","centerDiv");
+newDiv.setAttribute("id", "centerDiv");
 
-function centerContent(){
+function centerContent() {
 
-const card = document.createElement("div");
-card.setAttribute("class","card");
-card.innerHTML = "Test card"
-newDiv.appendChild(card);
-
-mainContent.appendChild(newDiv);
+    const card = document.createElement("div");
+    const p = document.createElement("p");
+    p.textContent = "Nepali Thakali food"
+    const img = new Image()
+    img.src = FoodBg;
+    card.setAttribute("class", "card");
+    card.appendChild(img)
+ card.appendChild(p)
+    newDiv.appendChild(card);
+    mainContent.appendChild(newDiv);
 
 
 }
 
-function Menu(){
+function Menu() {
     mainContent.innerHTML = "";
     centerText()
     centerContent()
-    centerContent()
-    centerContent()
+   
 
 
 }
