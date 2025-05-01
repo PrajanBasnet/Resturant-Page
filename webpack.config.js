8f85cb665e3f254f2cbe0a63a593338bd34c507e
prajwal <spiderbasnet140@gmail.com>
2025-05-01 13:22:17 +0545

    done

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
     },
     plugins:[
         new HtmlWebpackPlugin({
-            template:"./src/template.html",
-        })
+             template:"./src/template.html",
+        }) 
     ],
     module:{
         rules:[
